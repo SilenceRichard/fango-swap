69828edef0657ab2206dc39013efc18095dd41b1
SilenceRichard <silencerichard@163.com>
2025-01-02 13:24:33 +0800

    添加价格区间

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -3,7 +3,15 @@ import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200, // Optimize for deployment size
+      },
+    },
+  },
 };
 
 export default config;
