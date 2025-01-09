69a59d913eee60929cc94d83b4926d92a626cb69
SilenceRichard <silencerichard@163.com>
2025-01-09 23:22:08 +0800

    deploy sepolia

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,8 +1,16 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import { PRIVATE_KEY } from './.key';
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
+  networks: {
+    sepolia: {
+      chainId: 11155111,
+      url: "https://eth-sepolia.g.alchemy.com/v2/cEALgnMYS1N0l6ZOp7JawQmDytmSfLpJ",
+      accounts: [PRIVATE_KEY]
+    }
+  },
   solidity: {
     version: "0.8.28",
     settings: {
